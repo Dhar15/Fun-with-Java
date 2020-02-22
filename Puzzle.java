ed96891315202354687727d1921b3558cee8d6ae
Kshitij Dhar <39898582+Dhar15@users.noreply.github.com>
2020-02-22 12:01:31 +0530

    Update Puzzle.java

--- Puzzle.java
+++ Puzzle.java
@@ -1,27 +1,29 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JOptionPane;
-public class Puzzle extends Frame implements ActionListener{
-Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+import javax.swing.*;
+public class Puzzle extends JFrame implements ActionListener{
+JButton b1,b2,b3,b4,b5,b6,b7,b8,b9;
+
 Puzzle(){
 	super("Puzzle Game");
-	b1=new Button("5");
+	b1=new JButton("5");
 	b1.setBounds(50,100,40,40);
-	b2=new Button("6");
+	b2=new JButton("6");
 	b2.setBounds(100,100,40,40);
-	b3=new Button("3");
+	b3=new JButton("3");
 	b3.setBounds(150,100,40,40);
-	b4=new Button("2");
+	b4=new JButton("2");
 	b4.setBounds(50,150,40,40);
-	b5=new Button("1");
+	b5=new JButton("1");
 	b5.setBounds(100,150,40,40);
-	b6=new Button("8");
+	b6=new JButton("8");
 	b6.setBounds(150,150,40,40);
-	b7=new Button("7");
+	b7=new JButton("7");
 	b7.setBounds(50,200,40,40);
-	b8=new Button("4");
+	b8=new JButton("4");
 	b8.setBounds(100,200,40,40);
-	b9=new Button("");
+	b9=new JButton("");
 	b9.setBounds(150,200,40,40);
 	
 	b1.addActionListener(this);
